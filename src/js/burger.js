20fedddffacf8479bfaf7bdc4856e796ac1312ee
Sergey chaplin <chaplinsergey92@gmail.com>
2025-11-24 16:53:36 +0300

    added banner home

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,10 +2,25 @@ export function Burger() {
   const burger = document.getElementById("burger");
   const menu = document.getElementById("menu");
 
-  if (!burger || !menu) return; // безопасная проверка
+  if (!burger || !menu) return;
 
-  burger.addEventListener("click", () => {
+  burger.addEventListener("click", e => {
+    e.stopPropagation();
     burger.classList.toggle("active");
     menu.classList.toggle("active");
+    document.body.classList.toggle("active-menu");
+  });
+
+  // клик вне меню закрывает
+  document.addEventListener("click", e => {
+    if (
+      !menu.contains(e.target) &&
+      !burger.contains(e.target) &&
+      menu.classList.contains("active")
+    ) {
+      burger.classList.remove("active");
+      menu.classList.remove("active");
+      document.body.classList.remove("active-menu");
+    }
   });
 }
