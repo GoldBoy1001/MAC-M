050c3d7be74f7514c8589a0c88e142b3fd865331
GoldBoy1001 <chaplinsergey29@gmail.com>
2025-11-25 16:15:38 +0300

    added features

--- src/main.js
+++ src/main.js
@@ -8,4 +8,27 @@ import { Burger } from "./js/burger";
 
 document.addEventListener("DOMContentLoaded", () => {
   Burger();
+  const gear = document.querySelector(".gear");
+
+  let lastScrollY = window.scrollY;
+  let ticking = false;
+  let rotation = 0;
+
+  function onScroll() {
+    const currentScrollY = window.scrollY;
+    const delta = currentScrollY - lastScrollY;
+
+    rotation += delta * 0.5; // скорость вращения, можно регулировать
+    gear.style.transform = `rotate(${rotation}deg)`;
+
+    lastScrollY = currentScrollY;
+    ticking = false;
+  }
+
+  window.addEventListener("scroll", () => {
+    if (!ticking) {
+      window.requestAnimationFrame(onScroll);
+      ticking = true;
+    }
+  });
 });
