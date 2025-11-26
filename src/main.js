39728c706d071688c54c98f6199fd57f0ddb4d62
Sergey chaplin <chaplinsergey92@gmail.com>
2025-11-26 16:45:57 +0300

    adde slide and about

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
 import "normalize.css";
 
 import "./style.scss";
@@ -8,27 +11,36 @@ import { Burger } from "./js/burger";
 
 document.addEventListener("DOMContentLoaded", () => {
   Burger();
-  const gear = document.querySelector(".gear");
 
-  let lastScrollY = window.scrollY;
-  let ticking = false;
-  let rotation = 0;
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 3, // показывать 3 слайда
+    spaceBetween: 40, // расстояние между слайдами в пикселях
 
-  function onScroll() {
-    const currentScrollY = window.scrollY;
-    const delta = currentScrollY - lastScrollY;
-
-    rotation += delta * 0.5; // скорость вращения, можно регулировать
-    gear.style.transform = `rotate(${rotation}deg)`;
-
-    lastScrollY = currentScrollY;
-    ticking = false;
-  }
-
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next-index",
+      prevEl: ".swiper-button-prev-index",
+    },
+    // Можно добавить адаптив
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 2,
+      },
+      1740: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
   window.addEventListener("scroll", () => {
-    if (!ticking) {
-      window.requestAnimationFrame(onScroll);
-      ticking = true;
-    }
+    const gear = document.getElementById("gear-group");
+    const rotation = window.scrollY * 0.3; // скорость вращения
+    gear.style.transform = `rotate(${rotation}deg)`;
   });
 });
