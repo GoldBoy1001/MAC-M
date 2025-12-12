2a52011ab594b4169cd396294953d0a92a020244
Sergey chaplin <chaplinsergey92@gmail.com>
2025-12-12 16:35:27 +0300

    init catalog

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
+        catalog: resolve(__dirname, "catalog.html"),
       },
       output: {
         assetFileNames: "assets/[name].[ext]",
