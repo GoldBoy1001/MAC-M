0d7083406f98447801c6f8301b7645b9fa1a9712
Sergey chaplin <chaplinsergey92@gmail.com>
2025-12-12 16:45:11 +0300

    init catalog

--- src/js/moveTexts.js
+++ src/js/moveTexts.js
@@ -3,33 +3,34 @@ export function moveTexts() {
   const bannerBody = document.querySelector(".about-banner__body");
   const certificateBody = document.querySelector(".about-certificate__body");
   const BREAKPOINT = 480;
-
-  // Функция перемещения элемента
-  function moveBannerTexts(width) {
-    if (width < BREAKPOINT) {
-      // вставляем первым элементом в about-certificate__body
-      if (certificateBody.firstElementChild !== bannerTexts) {
-        certificateBody.insertBefore(bannerTexts, certificateBody.firstChild);
-      }
-    } else {
-      // вставляем последним элементом в about-banner__body
-      if (bannerBody.lastElementChild !== bannerTexts) {
-        bannerBody.appendChild(bannerTexts);
+  if (bannerTexts) {
+    // Функция перемещения элемента
+    function moveBannerTexts(width) {
+      if (width < BREAKPOINT) {
+        // вставляем первым элементом в about-certificate__body
+        if (certificateBody.firstElementChild !== bannerTexts) {
+          certificateBody.insertBefore(bannerTexts, certificateBody.firstChild);
+        }
+      } else {
+        // вставляем последним элементом в about-banner__body
+        if (bannerBody.lastElementChild !== bannerTexts) {
+          bannerBody.appendChild(bannerTexts);
+        }
       }
     }
+
+    // Инициализация
+    moveBannerTexts(window.innerWidth);
+
+    // ResizeObserver следит за изменением размера окна
+    const resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        const width = entry.contentRect.width;
+        moveBannerTexts(width);
+      }
+    });
+
+    // наблюдаем за document.body
+    resizeObserver.observe(document.body);
   }
-
-  // Инициализация
-  moveBannerTexts(window.innerWidth);
-
-  // ResizeObserver следит за изменением размера окна
-  const resizeObserver = new ResizeObserver(entries => {
-    for (let entry of entries) {
-      const width = entry.contentRect.width;
-      moveBannerTexts(width);
-    }
-  });
-
-  // наблюдаем за document.body
-  resizeObserver.observe(document.body);
 }
